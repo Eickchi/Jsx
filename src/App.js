fd7c9b5d0d2e60b4b6a3f5648bb0607232c6560b
Eickchi <ahmedbouabda22@gmail.com>
2020-11-16 14:33:05 +0100

    Jsx Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+       <div className="App">
+            <div style={{border:solid ,'1px':black,maxWidth:'100vw',}}>
+
+              <h1 className="title red">Your name here</h1>
+
+              <br />
+
+              <img src={require("./public/imageInSrc.jpg")}  />
+
+              <br />
+
+              <img src={require("./public/imageInPublic.jpg")} />
+
+            </div>
+
+        <video width="320" height="240" controls>
+
+        <source src={require("myVideo.mp4")} type="video/mp4" / >
+
+        </video>
+      </div>
+    );
+  }
 }
-
 export default App;
